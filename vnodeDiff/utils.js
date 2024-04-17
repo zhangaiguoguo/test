b1c49ae911dd16f3928ef4c582fd798a7d3db5c9
zhangaiguoqweabc <1346795213zAg>
2024-04-17 19:46:33 +0800

    修复

--- vnodeDiff/utils.js
+++ vnodeDiff/utils.js
@@ -47,11 +47,14 @@ export function transFormArray(target) {
   return Array.isArray(target) ? target : [target];
 }
 
-const NODE = typeof Node ? Node : {
-  TEXT_NODE: 3,
-  ELEMENT_NODE: 1,
-  COMMENT_NODE: 8,
-  FRAGMENT_NODE: 21,
+const NODECOUNT = 1
+
+const NODE = {
+  TEXT_NODE: NODECOUNT << 1,
+  ELEMENT_NODE: NODECOUNT << 2,
+  COMMENT_NODE: NODECOUNT << 3,
+  FRAGMENT_NODE: NODECOUNT << 4,
+  COMPONENT_NODE: NODECOUNT << 5,
 }
 
 export const TEXT_NODE = NODE.TEXT_NODE
@@ -61,6 +64,8 @@ export const ELEMENT_NODE = NODE.ELEMENT_NODE
 export const COMMENT_NODE = NODE.COMMENT_NODE
 
 export const FRAGMENT_NODE = NODE.IFRAGMENT_NODE || 21
+
+export const COMPONENT_NODE = NODE.COMPONENT_NODE || 42
 
 function toSymbol(val) {
   return Symbol(val)
@@ -74,91 +79,43 @@ export const TEXT = toSymbol("text")
 
 export const FRAGMENT = toSymbol("fragment")
 
+export const COMPONENT = toSymbol("component")
+
 export const ELEMENT_TYPE = "elementType"
+
+export const is_v_node_ref = "__is_v_node"
 
 export function getNodeType(node) {
   return node && node[ELEMENT_TYPE]
-}
-
-export const isTextContentNode = (n) => {
-  const type = getNodeType
-  return type === TEXT || type === COMMENT
 }
 
 export function isFragment(target) {
   return getNodeType(target) === FRAGMENT
 }
 
-export function isDiffFragmentFlag(n1, n2) {
-  const flag = isFragment(n1)
-  const flag2 = isFragment(n2)
-  return ((flag && !flag2) || (!flag && flag2))
-}
-
-export function isSameKey(n1, n2) {
-  return (n1 && n1[KEY] !== null && n2 && n2[KEY] === null) || ((n1 && n1[KEY] === null && n2 && n2[KEY] !== null))
-}
-
-export function isSameNodeType2(n1, n2) {
-  if (isSameNodeType(n1, n2)) {
-    return true
-  }
-  return n1[KEY] !== n2[KEY]
-}
-
 export function clearArrayValue(target) {
   target.splice(0, target.length)
 }
 
-export const KEY_PERM_KEY = "KEY_PERM";
-export const KEY_PERM_N1_PERM = 0b000001;
-export const KEY_PERM_N2_PERM = 0b000010;
-export const KEY_PERM_N_PERM = 0b0000100;
-
-export function diffKey$(n1, n2) {
-  const diffKeyState = this.diffKeyState;
-  var sub = diffKeyState.find(
-    (item) =>
-      (item.n1 === n1 && item.n2 === n2) || (item.n1 === n2 && item.n2 === n1)
-  );
-  if (sub) {
-    return sub[KEY_PERM_KEY];
-  }
-  var KEY_PERM = 0b000000;
-
-  if (n1[KEY] !== null && n1[KEY] !== void 0) {
-    KEY_PERM = KEY_PERM | KEY_PERM_N1_PERM;
-  }
-
-  if (!n2) return KEY_PERM;
-
-  if (n2 && n2[KEY] !== null && n2[KEY] !== void 0) {
-    KEY_PERM = KEY_PERM | KEY_PERM_N2_PERM;
-  }
-
-  if (
-    isCurrentScopeExist(KEY_PERM, KEY_PERM_N1_PERM) &&
-    isCurrentScopeExist(KEY_PERM, KEY_PERM_N2_PERM) &&
-    n1[KEY] === n2[KEY]
-  ) {
-    KEY_PERM = KEY_PERM | KEY_PERM_N_PERM;
-  }
-  this.diffKeyState.push({ n1, n2, [KEY_PERM_KEY]: KEY_PERM });
-  return KEY_PERM;
+export function isObject(target) {
+  return toRawType(target) === "Object"
 }
 
+export const isArray = Array.isArray
 
-export function isSpecialLabel(tag) {
-  return tag === "script" || tag === "style";
+export function validateVnodeTypeShageFlag(type) {
+  if (typeof type === "object" && type) {
+    return COMPONENT_NODE
+  }
+  if (/[^a-z-]/ig.test(type)) {
+    return TEXT_NODE
+  }
+  if (type === null) {
+    return FRAGMENT_NODE
+  }
+  return ELEMENT_NODE
 }
 
-export function isCurrentScopeExist(cexits, current) {
-  return (cexits & current) === current;
-}
-
-export function isSameNodeType(n1, n2) {
-  if (n1.tag === n2.tag || n1[KEY] === n2[KEY] || getNodeType(n1) === getNodeType(n2)) {
-    return true
-  };
-  return false
+export function isVnode(node){
+  return node && !!node[is_v_node_ref]
 }
